afb725243c2ccf3cde299a6c6f818458a6198016
Logan McNulty <logan.mcnulty@zmfs.local>
2019-11-21 21:43:04 -0500

    cleaned up backend, merged welcome page with main page, adjusted user models. Will update APIs next

--- models/user.js
+++ models/user.js
@@ -16,6 +16,27 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    studentName: {
+      type: DataTypes.STRING,
+      // FIX THIS LATER, FOR TESTING PURPOSES NULL NOW
+      allowNull: true,
+      validate: {
+        len: [1]
+      }
+    },
+    studentGradeLevel: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    reminderSchedule: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    userBOOL: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      defaultValue: true
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
@@ -25,7 +46,11 @@ module.exports = function(sequelize, DataTypes) {
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
   User.addHook("beforeCreate", function(user) {
-    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
   });
   return User;
 };
--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
+
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 
-// Setting up port and requiring models for syncing
+// Setting up express app
 var PORT = process.env.PORT || 8080;
+var app = express();
+
+// Requiring our models for syncing
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Static directory
 app.use(express.static("public"));
+
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -25,6 +33,10 @@ require("./routes/api-routes.js")(app);
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
